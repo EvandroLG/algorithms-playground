b97e12fa890bffde8dd0f9df03fd3090894c0626
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-06-10 23:09:34 +0200

    adds tests to array-list

--- src/arrays/array-list.js
+++ src/arrays/array-list.js
@@ -1,3 +1,8 @@
+/*
+  ArrayList
+  Write an ArrayList class using Object. Your ArrayList should have the following methods: get, push, pop and delete.
+*/
+
 class ArrayList {
   constructor() {
     this.length = 0;
@@ -30,18 +35,33 @@ class ArrayList {
       return;
     }
 
-    if (index === this.length - 1) {
-      delete this.items[index];
-      return;
-    }
+    const value = this.items[index];
+    let currentIndex = index;
 
-    const currentIndex = index; // 1
     while (this.items.hasOwnProperty(currentIndex + 1)) {
       this.items[currentIndex] = this.items[++currentIndex];
     }
 
     delete this.items[this.length - 1];
-
     this.length--;
+
+    return value;
   }
 }
+
+const assert = require('assert');
+
+const arr = new ArrayList();
+arr.push(10);
+arr.push(20);
+arr.push(30);
+arr.push(5);
+
+assert.equal(arr.length, 4);
+assert.equal(arr.pop(), 5);
+assert.equal(arr.length, 3);
+assert.equal(arr.remove(0), 10);
+assert.equal(arr.length, 2);
+assert.equal(arr.get(0), 20);
+assert.equal(arr.get(1), 30);
+assert.equal(arr.get(2), undefined);
