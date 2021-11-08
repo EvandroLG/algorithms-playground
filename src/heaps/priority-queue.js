46747f99523b61da4299c7c39f662fa87711d9c7
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-11-08 11:19:14 -0300

    update priority queue

--- src/heaps/priority-queue.js
+++ src/heaps/priority-queue.js
@@ -6,8 +6,8 @@ const swap = (arr, i, j) => {
 
 class PriorityQueue {
   constructor(compare) {
-    this.compare = (a, b) => compare(a, b) > 0;
     this.heap = [];
+    this.compare = compare;
   }
 
   size() {
@@ -15,72 +15,79 @@ class PriorityQueue {
   }
 
   peek() {
-    return this.heap[0];
+    return this.heap.length ? this.heap[0] : undefined;
   }
 
   add(element) {
     this.heap.push(element);
 
-    if (this.heap.length > 1) {
+    if (this.heap.length) {
       this.moveUp(this.heap.length - 1);
     }
   }
 
   poll() {
-    if (this.heap.length <= 1) {
-      return this.hep.pop();
-    }
-
-    swap(0, this.heap.length - 1);
-    const removed = this.heap.pop();
-
-    if (this.heap.length > 1) {
-      this.moveDown(0);
-    }
-
-    return removed;
-  }
-
-  moveDown(index) {
-    if (index >= this.heap.length - 1) return;
-
-    const child = this.getChild(index);
-    if (!child) return;
-
-    if (this.compare(this.heap[index], this.heap[child])) {
-      swap(index, child);
-      this.moveDown(child);
-    }
-  }
-
-  getChild(index) {
-    let left = index * 2 + 1;
-    if (left >= this.heap.length) left = null;
-
-    let right = index * 2 + 2;
-    if (right >= this.heap.length) right = null;
-
-    if (left && right) {
-      return this.compare(this.heap[left], this.heap[right]) ? right : left;
-    }
-
-    if (right) {
-      return right;
-    }
-
-    if (left) {
-      return left;
-    }
-  }
-
-  moveUp(index) {
-    if (index === 0) {
+    if (!this.heap.length) {
       return;
     }
 
-    const parent = Math.floor(index / 2);
-    if (this.compare(this.heap[parent], this.heap[index])) {
-      swap(parent, index);
+    const value = this.heap[0];
+
+    if (this.heap.length === 1) {
+      this.heap.length = 0;
+    } else {
+      this.heap[0] = this.heap[this.heap.length - 1];
+      this.heap.length -= 1;
+
+      if (this.heap.length > 1) {
+        this.moveDown(0);
+      }
+    }
+
+    return value;
+  }
+
+  moveDown(idx) {
+    const left = 2 * idx + 1;
+    const right = 2 * idx + 2;
+    const leftSwappable = !!this.compare(this.heap[idx], this.heap[left]);
+
+    if (right >= this.heap.length) {
+      if (leftSwappable) {
+        swap(this.heap, idx, left);
+      }
+
+      return;
+    }
+
+    const rightSwappable = !!this.compare(this.heap[idx], this.heap[right]);
+
+    if (leftSwappable && rightSwappable) {
+      const comparison = this.compare(this.heap[left], this.heap[right]);
+
+      if (comparison < 0) {
+        swap(this.heap, idx, left);
+        this.moveDown(left);
+      } else {
+        swap(this.heap, idx, right);
+        this.moveDown(right);
+      }
+    } else {
+      if (leftSwappable) {
+        swap(this.heap, idx, left);
+        this.moveDown(left);
+      } else {
+        swap(this.heap, idx, right);
+        this.moveDown(right);
+      }
+    }
+  }
+
+  moveUp(idx) {
+    const parent = Math.floor((idx - 1) / 2);
+
+    if (this.compare(this.heap[parent], this.heap[idx])) {
+      swap(this.heap, idx, parent);
       this.moveUp(parent);
     }
   }
