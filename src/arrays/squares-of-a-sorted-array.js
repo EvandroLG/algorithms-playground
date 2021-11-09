66acd08c3bbef18a9cbab9c2e79a5def7ac94d6e
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-11-09 12:46:18 -0300

    improve solution of `squares of a sorted array` problem

--- src/arrays/squares-of-a-sorted-array.js
+++ src/arrays/squares-of-a-sorted-array.js
@@ -1,10 +1,33 @@
 /*
   SquaresOfASortedArray
+
   Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+  Example:
+  Input: [-4, -1, 0, 3, 10]
+  Output: [0, 1, 9, 16, 100]
 */
 
 function squaresOfASortedArray(arr) {
-  return arr.map((n) => Math.pow(n, 2)).sort((a, b) => a - b);
+  const output = [];
+  const size = arr.length;
+  let p1 = 0;
+  let p2 = size - 1;
+
+  for (let i = size - 1; i >= 0; i--) {
+    let result = arr[p2];
+
+    if (Math.abs(arr[p1]) > Math.abs(arr[p2])) {
+      result = arr[p1];
+      p1++;
+    } else {
+      p2--;
+    }
+
+    output[i] = Math.pow(result, 2);
+  }
+
+  return output;
 }
 
 const assert = require('assert');
