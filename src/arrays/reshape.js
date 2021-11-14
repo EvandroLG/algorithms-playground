dfd9601b16f6cc96374b80189c653b0232471f56
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-11-14 15:00:02 -0300

    solve a new array problem

---
+++ src/arrays/reshape.js
@@ -0,0 +1,69 @@
+/*
+  Reshape
+
+  In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+  You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+  The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+  If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+  Example:
+  Input: [[1,2],[3,4]], 1, 4
+  Output: [[1,2,3,4]]
+*/
+
+function flat(arr) {
+  const output = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      output.push(...flat(item));
+    } else {
+      output.push(item);
+    }
+  }
+
+  return output;
+}
+
+function reshape(arr, r, c) {
+  const flatted = flat(arr);
+  const output = [];
+  let tmp = [];
+  let rows = 0;
+  let cols = 0;
+  let idx = 0;
+
+  while (idx < flatted.length) {
+    const item = flatted[idx];
+    tmp.push(item);
+    idx++;
+
+    if (cols < c - 1) {
+      cols++;
+    } else {
+      cols = 0;
+      rows++;
+      output.push(tmp);
+      tmp = [];
+    }
+  }
+
+  if (rows !== r || cols > 0) {
+    return arr;
+  }
+
+  return output;
+}
+
+const assert = require('assert');
+assert.deepEqual(
+  reshape(
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    1,
+    4
+  ),
+  [[1, 2, 3, 4]]
+);
