3ab6fe017cd2baf82b9becd689ea8cac4698d345
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-12-16 10:50:19 -0300

    solve a new linked list problem

---
+++ src/linked-list/swap-nodes-in-pairs.js
@@ -0,0 +1,59 @@
+/*
+  Swap Nodes In Pairs
+
+  Given a linked list, swap every two adjacent nodes and return its head.
+  You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+  Example:
+  Input: [1,2,3,4]
+  Output: [2, 1, 4, 3]
+*/
+
+function swapNodesInPairs(head, prev = null) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  const tmp = head;
+  head = head.next;
+  const next = head.next;
+  head.next = tmp;
+  head.next.next = next;
+
+  if (prev) {
+    prev.next = head;
+  }
+
+  swapNodesInPairs(head.next.next, head.next);
+
+  return head;
+}
+
+const assert = require('assert');
+const head = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+assert.deepEqual(swapNodesInPairs(head), {
+  value: 2,
+  next: {
+    value: 1,
+    next: {
+      value: 4,
+      next: {
+        value: 3,
+        next: null,
+      },
+    },
+  },
+});
