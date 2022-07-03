1a3929922d0bdf04aec0800835bf9e9fce8de83a
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-07-03 11:45:15 +0200

    solves a new string problem

---
+++ src/strings/uncompress-string.js
@@ -0,0 +1,54 @@
+/*
+  Uncompress String
+
+  Given a compressed string, return its original form.
+  1) a number k followed by a pair of parenthesis, meaning to repeat the substring inside the parenthesis by k times, k is positive integer.
+  2) inputs are guaranteed to be valid input like above example, there is no numerical digit in original form.
+
+  Example:
+  Input: '3(ab2(c))'
+  Output: 'abccabccabcc'
+*/
+
+function uncompressString(str) {
+  const numberStack = [];
+  const stringStack = [];
+  const currentString = [];
+  let k = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i];
+
+    if (isDigit(c)) {
+      const digit = [c];
+
+      while (isDigit(str[i + 1])) {
+        digit.push(str[i + 1]);
+        i++;
+      }
+
+      k = +digit.join('');
+    } else if (c === '(') {
+      numberStack.push(k);
+      stringStack.push(currentString.join(''));
+      currentString.length = 0;
+    } else if (c === ')') {
+      const number = numberStack.pop();
+      const str = stringStack.pop();
+      const decoded = currentString.join('').repeat(number);
+      currentString.length = 0;
+      currentString.push([str, decoded].join(''));
+    } else {
+      currentString.push(c);
+    }
+  }
+
+  return currentString.join('');
+}
+
+function isDigit(c) {
+  return /\d/.test(c);
+}
+
+const assert = require('assert');
+assert.equal(uncompressString('3(ab2(c))'), 'abccabccabcc');
