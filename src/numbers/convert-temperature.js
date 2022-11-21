6d34f83dec5a7a5959cd8d5efbf0e55f8a17f978
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-11-21 17:20:47 +0100

    solves a new math problem

---
+++ src/numbers/convert-temperature.js
@@ -0,0 +1,18 @@
+/*
+  Convert Temperature
+
+  You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+  You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+  Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+  Example:
+  Input: 36.50
+  Output: [309.65000, 97.70000]
+*/
+
+function convertTemperature(celsius) {
+  return [celsius + 273.15, celsius * 1.8 + 32.0];
+}
+
+const assert = require('assert');
+assert.ok(convertTemperature());
