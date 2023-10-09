cb7675559822e069ca353a9da54e0ef6e587342b
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2023-10-09 21:42:18 +0200

    solves a new number problem

---
+++ src/numbers/divisor-game.js
@@ -0,0 +1,21 @@
+/*
+  Divisor Game
+
+  Alice and Bob take turns playing a game, with Alice starting first.
+  Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+  - Choosing any x with 0 < x < n and n % x == 0.
+  - Replacing the number n on the chalkboard with n - x.
+  Also, if a player cannot make a move, they lose the game.
+  Return true if and only if Alice wins the game, assuming both players play optimally.
+
+  Example:
+  Input: 3
+  Output: false
+*/
+
+function divisorGame(n) {
+  return n % 2 === 0;
+}
+
+const assert = require('assert');
+assert.ok(!divisorGame(3));
