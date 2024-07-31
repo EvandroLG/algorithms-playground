7940ea977ff5d9db725f8b4e4b5df2b731d17d0e
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-07-31 10:10:22 +0200

    improves performance of an array problem

--- src/arrays/move-all-zeros-to-the-right.js
+++ src/arrays/move-all-zeros-to-the-right.js
@@ -11,24 +11,19 @@
 */
 
 function moveAllZerosToTheRight(arr) {
-  const size = arr.length;
-  let count = 0;
+  let last = 0;
 
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
+      const tmp = arr[last];
+      arr[last] = arr[i];
+      arr[i] = tmp;
+      last++;
     }
   }
-
-  while (count < size) {
-    arr[count] = 0;
-    count++;
-  }
-
-  return arr;
 }
 
 const assert = require('assert');
 const arr = [1, 0, 0, 2, 5, 0];
-assert.deepEqual(moveAllZerosToTheRight(arr), [1, 2, 5, 0, 0, 0]);
+moveAllZerosToTheRight(arr);
+assert.deepEqual(arr, [1, 2, 5, 0, 0, 0]);
